fadaac3ed7b72502901ebfc76761c815cb00e374
Michal Vranec <vranec.michal@gmail.com>
2016-08-11 18:46:12 +0200

    duration component done

--- app/pages/my-time/new-booking/new-booking.ts
+++ app/pages/my-time/new-booking/new-booking.ts
@@ -1,26 +1,31 @@
 import {Component} from '@angular/core';
-import {ViewController, ModalController, PickerController,ActionSheetController} from 'ionic-angular';
+import {ViewController, ModalController, PickerController, ActionSheetController} from 'ionic-angular';
 import * as moment from 'moment';
 
 import {MyTimeService} from '../my-time.service';
 import {TaskSelection} from '../../../components/task-selection/task-selection';
+import {DurationTypePipe} from '../../../pipes/duration-type-pipe';
 
 enum DurationType{
-    HOURS = <any>'hours', MINUTES = <any>'minutes'
+    HOURS = <any>'hours', MINUTES = <any>'minutes', NONE = <any>'none'
 }
 
 @Component({
     templateUrl: 'build/pages/my-time/new-booking/new-booking.html',
-    providers: [MyTimeService]
+    providers: [MyTimeService],
+    pipes: [DurationTypePipe]
 })
 export class NewBooking {
 
-    bookingDate: String;
+    bookingDate: string;
     duration: number;
     durationType: DurationType = DurationType.HOURS;
+    durationTemp: string;
 
-    constructor(private actionSheetController : ActionSheetController ,private pickerCtrl: PickerController, private modalCtrl: ModalController, private viewCtrl: ViewController, private myTimeService: MyTimeService) {
+    constructor(private actionSheetController: ActionSheetController, private pickerCtrl: PickerController, private modalCtrl: ModalController, private viewCtrl: ViewController, private myTimeService: MyTimeService) {
         this.bookingDate = moment().format('DD.MM.YYYY');
+        this.durationTemp = '0.25';
+        this.duration = 0.25;
     }
 
     cancel() {
@@ -45,30 +50,39 @@ export class NewBooking {
             text: 'Done',
             handler: (data) => {
                 this.duration = data.duration.value;
+                console.log('before',data.duration.value);
+                this.durationTemp = this.durationType == DurationType.MINUTES ? (data.duration.value / 60).toFixed(2) : data.duration.value.toString();
+                console.log('after',this.durationTemp);
             }
         });
         picker.addColumn({
             name: 'duration',
-            options: this.generateDurationValues('minutes')
+            options: this.generateDurationValues(this.durationType)
         });
         picker.present();
     }
 
-    openDurationTypeActionSheet(){
+    openDurationTypeActionSheet() {
         let actionSheet = this.actionSheetController.create({
             title: 'Duration Type',
             buttons: [
                 {
                     text: 'Hours',
                     handler: () => {
-                        this.durationType = DurationType.HOURS;
+                        if (this.durationType == DurationType.MINUTES) {
+                            this.duration /= 60;
+                            this.durationType = DurationType.HOURS
+                        }
                     }
-                },{
+                }, {
                     text: 'Minutes',
                     handler: () => {
-                        this.durationType = DurationType.MINUTES;
+                        if (this.durationType == DurationType.HOURS) {
+                            this.duration *= 60;
+                            this.durationType = DurationType.MINUTES
+                        }
                     }
-                },{
+                }, {
                     text: 'Cancel',
                     role: 'cancel',
                 }
@@ -77,14 +91,18 @@ export class NewBooking {
         actionSheet.present();
     }
 
+    selectedDurationSegment(value) {
+        this.duration = this.durationType == DurationType.MINUTES ? value * 60 : value;
+    }
+
     //value is always in hours but text is either in hours or minutes
-    private generateDurationValues(type) {
+    private generateDurationValues(durationType: DurationType) {
         let array = [];
-        let count = type === 'hours' ? 0.25 : 1;
-        const max = type === 'hours' ? 24 : 1415.4;
+        let count = durationType == DurationType.HOURS ? 0.25 : 1;
+        const max = durationType == DurationType.HOURS ? 24 : 1415.4;
         let i = count;
         for (i; i < max; i += count) {
-            array.push({text: type === 'hours' ? i.toFixed(2) : i, value: i});
+            array.push({text: durationType == DurationType.HOURS ? i.toFixed(2) : i, value: i});
         }
         return array;
     }
