9c9dc7fb6b10815a46841298e821c7b6f6e40fa8
Michal Vranec <vranec.michal@gmail.com>
2016-08-26 11:07:49 +0200

    style working step detail

--- app/pages/my-time/working-step-detail/working-step-detail.ts
+++ app/pages/my-time/working-step-detail/working-step-detail.ts
@@ -1,9 +1,12 @@
 import {Component} from '@angular/core';
 import {NavParams} from 'ionic-angular';
+import {DateFormatPipe} from 'angular2-moment';
 
+import {DurationPipe} from '../../../pipes/duration-pipe';
 
 @Component({
-    templateUrl: 'build/pages/my-time/working-step-detail/working-step-detail.html'
+    templateUrl: 'build/pages/my-time/working-step-detail/working-step-detail.html',
+    pipes:[DateFormatPipe,DurationPipe]
 })
 export class WorkingStepDetail {
 
